742119ff7246ae17cd9f5d87aeacc6296ef0e398
Victation <victorshlima@gmail.com>
2023-08-30 20:22:55 -0300

    11

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,8 +18,47 @@ function App() {
         >
           Learn React
         </a>
+
+      <div>
+        <p> Olá </p>      
+      </div>
       </header>
+      <body>
+
+      <div>
+        <p> I'm am the body </p>      
+      </div>
+      </body>
     </div>
+
+
+    <div className="App2">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         {1 +1 } Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+
+      <div>
+        <p> Olá </p>      
+      </div>
+      </header>
+      <body>
+
+      <div>
+        <p> I'm am the body </p>      
+      </div>
+      </body>
+    </div>
+    </>
   );
 }
 
