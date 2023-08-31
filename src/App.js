1c39edce2cd7cfcd2e849f5d30d61855bc72eb7f
Victation <victorshlima@gmail.com>
2023-08-30 21:37:59 -0300

    14

--- src/App.js
+++ src/App.js
@@ -3,31 +3,51 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
+
+
+  //THIS is a class FIELD, this remove the necessity of use constructors  
   state = {
-    name: 'Otávio Miranda',
+    name: 'Vic',
     counter: 0
-  };
-
-  handlePClick = () => {
-    this.setState({ name: 'Júnior' });
   }
 
-  handleAClick = (event) => {
-    event.preventDefault();
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
-  }
+  // class fields made me depreciated 
+  // constructor(props){
+  //   super(props);
+  //   this.state = {
+  //     name: 'Victor',
+  //     counter: 0
+  //   }
+  //   // this.handlePClick = this.handlePClick.bind(this)
+  // }
+
+    handlePClick = () => { // OLD way to GET the variable
+       const {name} = this.state;  //name -  only works with bind in constructo, 
+      console.log(`<p> clicado`)  //but using a class field and arow functuions this is not necessy anymore
+      this.setState({name : 'Junior'})
+    }
+
+    handleAClick = (event) => {
+      event.preventDefault();
+      
+      const {counter} = this.state;
+      this.setState({counter: counter +1})
+   }
 
   render() {
+
+    const nome  = this.state.nome
+
     const { name, counter } = this.state;
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handlePClick}>
-            {name} {counter}
-          </p>
+        <p onClick={this.handlePClick}>
+          {name}
+        </p>
+
           <a
             onClick={this.handleAClick}
             className="App-link"
